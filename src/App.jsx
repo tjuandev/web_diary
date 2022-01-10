6b31e803452432af900654e85aa927313f707d14
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-10 18:43:36 -0300

    Creating isBlockActive Function

--- src/App.jsx
+++ src/App.jsx
@@ -92,6 +92,14 @@ const toggleMark = (editor, format) => {
   }
 };
 
+const isBlockActive = (editor, format) => {
+  const [match] = Editor.nodes(editor, {
+    match: (n) => n.type === format,
+  });
+
+  return match;
+};
+
 const isMarkActive = (editor, format) => {
   const marks = Editor.marks(editor);
   return marks ? marks[format] === true : false;

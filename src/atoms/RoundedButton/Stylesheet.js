805ac89859a373cde2e9aa3159bae2a15929ef43
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-10 21:54:40 -0300

    Styling RoundedButton Atom

---
+++ src/atoms/RoundedButton/Stylesheet.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const RoundedButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 10px;
+
+  width: 45px;
+  height: 45px;
+
+  background: #f4f4f4;
+  box-shadow: -2px -2px 5px #ffffff, 2px 2px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 50%;
+`;

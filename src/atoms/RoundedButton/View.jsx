0bc69bb92d63ae4d4c9a194f12493ab9bedeee66
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-10 21:54:25 -0300

    Creating RoundedButton Atom

---
+++ src/atoms/RoundedButton/View.jsx
@@ -0,0 +1,11 @@
+import { RoundedButton } from "./Stylesheet";
+
+const View = ({ children }) => {
+  return (
+    <RoundedButton>
+      {children}
+    </RoundedButton>
+  );
+};
+
+export default View;

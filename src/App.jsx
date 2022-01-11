fa425e26d03811e4baac577a121a18a00644e4b0
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-10 22:27:19 -0300

    Creating of Toolbar organism

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { RoundedButton } from "./atoms";
+import { Toolbar } from "organisms";
 import GlobalStyles from "./utils/styles/GlobalStyles";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <RoundedButton />
+      <Toolbar />
     </>
   );
 };
--- src/organisms/Toolbar/View.jsx
+++ src/organisms/Toolbar/View.jsx
@@ -1,9 +1,19 @@
-import {BarContainer} from './Stylesheet';
+import { RoundedButton } from "atoms";
+import { editorToolbar } from "utils/constants";
+import { BarContainer } from "./Stylesheet";
 
 const View = () => {
   return (
-    <BarContainer></BarContainer>
-  )
-}
+    <BarContainer style={{ display: "flex", padding: "2rem" }}>
+      {Object.entries(editorToolbar).map(([key, value]) => {
+        return (
+          <RoundedButton id={key} key={key} style={{ margin: "0.5rem" }}>
+            {value}
+          </RoundedButton>
+        );
+      })}
+    </BarContainer>
+  );
+};
 
 export default View;

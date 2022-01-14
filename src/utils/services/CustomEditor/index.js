012f886353553c124487ff648bf06435bde8672c
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-13 21:57:11 -0300

    Creating block functions in CustomEditor

--- src/utils/services/CustomEditor/index.js
+++ src/utils/services/CustomEditor/index.js
@@ -1,4 +1,4 @@
-import { Editor } from "slate";
+import { Editor, Element, Transforms } from "slate";
 
 export const isMarkActive = (editor, format) => {
   const marks = Editor.marks(editor);
@@ -13,4 +13,39 @@ export const toggleMark = (editor, format) => {
   } else {
     Editor.addMark(editor, format, true);
   }
+};
+
+export const isBlockActive = (editor, format) => {
+  const { selection } = editor;
+  if (!selection) return false;
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: (n) =>
+        !Editor.isEditor(n) && Element.isElement(n) && n.type === format,
+    })
+  );
+
+  return match;
+};
+
+export const getLinkUrl = () => {
+  const url = prompt("Please, type your URL:");
+
+  return url;
+};
+
+export const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(editor, format);
+
+  const newProperties = {
+    type: isActive ? "default" : format,
+    href: isActive ? undefined : format === "link" ? getLinkUrl() : "#",
+  };
+
+  Transforms.setNodes(editor, newProperties, {
+    match: (n) => Editor.isBlock(editor, n),
+    split: true,
+  });
 };

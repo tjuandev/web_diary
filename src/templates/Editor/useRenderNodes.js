601ba24d45b1fca3070d418025daf3b9e28f1b40
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-01-15 11:39:39 -0300

    Extracting Nodes Components to a new File

--- src/templates/Editor/useRenderNodes.js
+++ src/templates/Editor/useRenderNodes.js
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import { removePartOfString } from "utils/lib/String";
+import { Leaf, CodeElement, DefaultElement } from "./Nodes";
 
 const useRenderNodes = () => {
   const renderLeaf = useCallback((props) => {
@@ -16,62 +16,6 @@ const useRenderNodes = () => {
       }
     }
   }, []);
-
-  const Leaf = (props) => {
-    const { children, attributes, leaf } = props;
-
-    const leafFlag = (format, defaultValue = "") =>
-      leaf[format] ? format : defaultValue;
-
-    const style = {
-      fontWeight: leafFlag("bold"),
-      fontStyle: leafFlag("italic"),
-      textDecoration: `${leafFlag("line-through")} ${leafFlag("underline")}`,
-    };
-
-    if (leaf.link) {
-      return <LinkLeaf style={style} url={leaf.url} {...props} />;
-    }
-
-    return (
-      <span {...attributes} style={style}>
-        {children}
-      </span>
-    );
-  };
-
-  const LinkLeaf = ({ children, attributes, style, url }) => {
-    const textDecoration = style.textDecoration;
-    const hasUnderline = textDecoration.includes("underline");
-
-    return (
-      <span
-        {...attributes}
-        onMouseDown={() => window.open(url, "_blank")}
-        style={{
-          ...style,
-          color: "blue",
-          textDecoration: hasUnderline
-            ? removePartOfString(textDecoration, "")
-            : textDecoration + " underline",
-        }}
-      >
-        {children}
-      </span>
-    );
-  };
-
-  const CodeElement = ({ children, attributes }) => {
-    return (
-      <pre {...attributes}>
-        <code>{children}</code>
-      </pre>
-    );
-  };
-
-  const DefaultElement = ({ children, attributes }) => {
-    return <p {...attributes}>{children}</p>;
-  };
 
   return [renderLeaf, renderElement];
 };

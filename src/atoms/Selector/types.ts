40cef5618ee8c6cf2d178f9af812f1b7d2cbe0cb
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-03-23 21:17:48 -0300

    Creating SingleCustomValueProps Type

--- src/atoms/Selector/types.ts
+++ src/atoms/Selector/types.ts
@@ -1,4 +1,4 @@
-import { MenuProps, Props } from "react-select";
+import { MenuProps, Props, SingleValueProps } from "react-select";
 
 export type SelectorProps = Props & {
   width?: string | number;
@@ -7,4 +7,12 @@ export type SelectorProps = Props & {
 
 export type MenuCustomProps = MenuProps & {
   isHorizontal?: boolean;
+};
+
+export type SingleCustomValueProps = SingleValueProps & {
+  data: {
+    value: string;
+    label: string;
+  };
+  isHighlighter?: boolean;
 };

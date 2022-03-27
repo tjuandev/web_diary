dc6eec1d678b6896dafe9676ebefd2a9acfb979b
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-03-27 13:16:06 -0300

    passing console to debug

--- src/organisms/Toolbar/Stylesheet.ts
+++ src/organisms/Toolbar/Stylesheet.ts
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
 export const BarContainer = styled.nav`
+  width: fit-content;
+  height: fit-content;
+
   display: flex;
-  padding: 2rem;
+  align-items: center;
+  gap: 1rem;
+
+  padding: 12px;
+
+  position: fixed;
+
+  top: 85%;
+  left: 50%;
+
+  transform: translateX(-50%);
+
+  background: #f4f4f4;
+  box-shadow: -4px -4px 4px #f8f8f8, 4px 4px 8px #e4e4e4;
+  border-radius: 50px;
 `;

8fcb6f105717b0077a8addcd6754a70772b2f03b
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-03-27 13:35:26 -0300

    teste again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import GlobalStyles from "utils/styles/GlobalStyles";
 import { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  console.log("aaaaaaaaaaaaaaaaaaa");
+
   return (
     <>
       <GlobalStyles />

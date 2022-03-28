009df1109b0c6a599fd27e51727a1545b9d67b93
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-03-28 19:53:01 -0300

    Styling and positioning toolbar

--- src/organisms/Toolbar/Stylesheet.ts
+++ src/organisms/Toolbar/Stylesheet.ts
@@ -2,5 +2,18 @@ import styled from "styled-components";
 
 export const BarContainer = styled.nav`
   display: flex;
-  padding: 2rem;
+  gap: 1rem;
+
+  background: #f4f4f4;
+  box-shadow: -4px -4px 4px #f8f8f8, 4px 4px 8px #e4e4e4;
+
+  padding: 0.8rem 1.5rem;
+
+  border-radius: 50px;
+
+  position: fixed;
+  top: calc(90% - 4rem);
+  left: 50%;
+
+  transform: translateX(-50%);
 `;

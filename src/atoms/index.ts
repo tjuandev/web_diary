5a7a0f50ac34cdda8d9bb11476701b06df1f9450
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-03-28 18:01:33 -0300

    asd

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -2,12 +2,14 @@ import dynamic from "next/dynamic";
 
 import * as RoundedButton from "./RoundedButton";
 
+/* eslint-disable */
+
 const HorizontalSelector = dynamic(
-  () => import("./Selector").then((mod) => mod.HorizontalSelector),
+  () => import("./Selector").then((mod) => mod.HorizontalSelector as any),
   { ssr: false }
 );
 const Selector = dynamic(
-  () => import("./Selector").then((mod) => mod.default),
+  () => import("./Selector").then((mod) => mod.default as any),
   { ssr: false }
 );
 

4e8de7986a1cb5212a5eceda0d904db642b52380
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-04-03 15:13:55 -0300

    Creating and styling EditorHeading template

---
+++ src/templates/EditorHeading/Stylesheet.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: linear-gradient(270deg, #ff7a00 0%, #ffa41b 100%);
+  box-shadow: 0px 4px 8px #e1e1e1;
+
+  h4 {
+    color: #f4f4f4;
+    font-weight: 500;
+
+    font-size: 1.25vw;
+  }
+
+  display: flex;
+  justify-content: center;
+
+  > div {
+    width: 53.5vw;
+  }
+
+  width: 100%;
+  padding: 1.5rem;
+`;
